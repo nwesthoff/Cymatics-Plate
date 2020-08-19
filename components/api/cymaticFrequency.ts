90528c95406e24690b639c3cfb148ca660d1b1ce
Nils Westhoff <nwesthoff@conceptivate.nl>
2020-08-19 12:24:00 +0200

    ✨ Add more potential frequencies

--- components/api/cymaticFrequency.ts
+++ components/api/cymaticFrequency.ts
@@ -1,4 +1,16 @@
 export const cymaticFrequency = () => {
-  const frequencies = ["250", "370", "564", "1140"];
+  const frequencies = [
+    "85",
+    "250",
+    "370",
+    "564",
+    "647",
+    "979",
+    "1140",
+    "1152",
+    "1272",
+    "2251",
+    "2533",
+  ];
   return frequencies[Math.floor(Math.random() * frequencies.length)];
 };
